e530a450f80c8aeb72eea50359a0e95669fddeb4
h8rry <harry@harrychen.net>
2016-12-15 17:04:03 +0800

    Use sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161209102128) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.boolean  "completed",  default: false
