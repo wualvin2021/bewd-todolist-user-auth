78d53fe95e65822713de80f4739f3f2f67f4a280
h8rry <harry@harrychen.net>
2016-12-20 18:50:16 +0800

    Private todolists

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   validates :content, length: { maximum: 200 }, presence: true
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe TasksController, type: :controller do
 
   describe 'GET /tasks' do
     it 'renders all tasks in JSON' do
-      task_1 = Task.create(content: 'Task #1')
-      task_2 = Task.create(content: 'Task #2')
+      user = FactoryGirl.create(:user)
+      task_1 = user.tasks.create(content: 'Task #1')
+      task_2 = user.tasks.create(content: 'Task #2')
 
       get :index
 
@@ -30,8 +31,39 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe 'GET /my_tasks' do
+    it 'renders all tasks of current user in JSON' do
+      user = FactoryGirl.create(:user)
+      session = user.sessions.create
+      @request.cookie_jar.signed['todolist_session_token'] = session.token
+      task = user.tasks.create(content: 'Task #1')
+
+      other_user = FactoryGirl.create(:user, username: 'other username')
+      other_user.tasks.create(content: 'Task #2')
+
+      get :index_by_current_user
+
+      expected_response = {
+        tasks: [
+          {
+            id: task.id,
+            content: task.content,
+            completed: task.completed,
+            created_at: task.created_at
+          }
+        ]
+      }
+
+      expect(response.body).to eq(expected_response.to_json)
+    end
+  end
+
   describe 'POST /tasks' do
     it 'renders newly created task in JSON' do
+      user = FactoryGirl.create(:user)
+      session = user.sessions.create
+      @request.cookie_jar.signed['todolist_session_token'] = session.token
+
       post :create, params: {
         task: {
           content: 'New Task'
@@ -53,7 +85,8 @@ RSpec.describe TasksController, type: :controller do
 
   describe 'DELETE /tasks/:id' do
     it 'renders success status' do
-      task = Task.create(content: 'Task Example')
+      user = FactoryGirl.create(:user)
+      task = user.tasks.create(content: 'Task Example')
 
       delete :destroy, params: { id: task.id }
 
@@ -64,7 +97,8 @@ RSpec.describe TasksController, type: :controller do
 
   describe 'PUT /tasks/:id/mark_complete' do
     it 'renders modified task' do
-      task = Task.create(content: 'Task Example')
+      user = FactoryGirl.create(:user)
+      task = user.tasks.create(content: 'Task Example')
 
       put :mark_complete, params: { id: task.id }
 
@@ -85,7 +119,8 @@ RSpec.describe TasksController, type: :controller do
 
   describe 'PUT /tasks/:id/mark_active' do
     it 'renders modified task' do
-      task = Task.create(content: 'Task Example', completed: true)
+      user = FactoryGirl.create(:user)
+      task = user.tasks.create(content: 'Task Example', completed: true)
 
       put :mark_active, params: { id: task.id }
 
